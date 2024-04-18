c117d6c4fd16cfbeab2ca963d248d1589bc89d90
sh1nn0on3 <sanogaku31@gmail.com>
2024-04-18 09:21:33 +0700

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 require "sidekiq/web"
 Rails.application.routes.draw do
+  devise_for :users
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => "/sidekiq"
   end
-  get 'pages#home'
+  # get 'pages/home'
+  root :to => "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
-
 end
