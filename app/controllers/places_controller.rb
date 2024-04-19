cafe7371f80bfa8622c7b10e825f3060d868890f
sh1nn0on3 <sanogaku31@gmail.com>
2024-04-19 09:44:38 +0700

    test

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,24 @@
+class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:title, addresses_attributes: [:id, :city, :street])
+  end
+end
